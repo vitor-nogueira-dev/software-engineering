2a0ccbe74df01458d4243c816de5358674dd0137
Vitor Nogueira <vslnogueira96@gmail.com>
2024-01-15 07:13:43 -0300

    Add main.js file with variable declarations and console logs

---
+++ dia_02/main.js
@@ -0,0 +1,26 @@
+console.log('Testando');
+
+var a = 10;
+var b = 20;
+var total = a + b;
+
+console.log(total);
+
+var nomePessoa = 'Vitor Nogueira';
+var NOMEPESSOA = 'Vander Nogueira';
+console.log(nomePessoa);
+
+var var1 = 1,
+  var2 = 2,
+  var3 = 3;
+
+
+var variave1;
+variave1 = 1;
+
+let book = 'Livro';
+book = 'Livro 2';
+
+const valor1 = 100;
+
+console.log(var1, var2, var3);
