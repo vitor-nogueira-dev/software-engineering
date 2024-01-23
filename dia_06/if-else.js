c440477102b86e3abb162be56a719ab45e30ebc0
Vitor Nogueira <vslnogueira96@gmail.com>
2024-01-23 07:20:34 -0300

    Add if-else statements to determine nationality and age

---
+++ dia_06/if-else.js
@@ -0,0 +1,15 @@
+const pais = "Brasil";
+
+if (pais !== "Brasil") {
+  console.log("Você é estrangeiro");
+} else {
+  console.log("Você é brasileiro");
+}
+
+const age = 17;
+
+if (age >= 18) {
+  console.log("Você é maior de idade");
+} else {
+  console.log("Você é menor de idade");
+}
