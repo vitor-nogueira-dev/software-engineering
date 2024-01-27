00ce70fbb28027a3477da16db691a141a440f2d1
Vitor Nogueira <vslnogueira96@gmail.com>
2024-01-27 09:07:53 -0300

    Add examples of different types of functions

---
+++ dia_09/examples-functions.js
@@ -0,0 +1,10 @@
+// regular function
+function sum(a, b) {
+  return a + b;
+}
+// anonymous function
+const sum = function (a, b) {
+  return a + b;
+};
+// arrow function
+const sum = (a, b) => a + b;
